a87ea4c3f14101653b7e710d2330ae49fb1ace9e
Zoes2 <v.meneses20.20@gmail.com>
2023-11-21 15:34:16 -0300

    validamos formulario y creamos estilo a mensaje de error

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -9,6 +9,8 @@ const CotizadorProvider = ({children}) => {
         year: '',
         plan: ''
     })
+
+    const [error, setError] = useState('')
     
     const handleChangeDatos = e => {
         setDatos({
@@ -23,7 +25,9 @@ const CotizadorProvider = ({children}) => {
         <CotizadorContext.Provider
             value={{
                 datos,
-                handleChangeDatos
+                handleChangeDatos,
+                error,
+                setError
             }}
         >
             {children}
